482b3531ab7d99e10e89fc08c4c66c03250640a0
scarter13 <scarter13@gmail.com>
2020-04-30 14:22:29 -0400

    some pseudocode and musings on next steps

--- calculator.js
+++ calculator.js
@@ -1,4 +1,12 @@
 
+// need to change console log on each box to move input to screen.  Also, need to revisit the screen itself to create a document type.  Assign a function to each button and use that function to append to display, i think, is the way to do this
+
+
+
+    let display = document.querySelector(".screen");
+    let newDisplay("div") y = document.createElement
+}
+
 
 function dis(val) {
     document.getElementById("screen").value+=val
