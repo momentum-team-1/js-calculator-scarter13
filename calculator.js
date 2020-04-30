ef007b137ea426f33c5767ca0a7b4fd5095c6b0a
scarter13 <scarter13@gmail.com>
2020-04-30 09:11:43 -0400

    console log works, at least

---
+++ calculator.js
@@ -0,0 +1,17 @@
+
+
+function dis(val) {
+    document.getElementById("screen").value+=val
+}
+
+function solve(){
+    let x = document.getElementById("screen").value
+    let y = eval(x)
+    document.getElementById("screen".value = y)
+}
+
+function clr(){
+    document.getElementById("screen").value = ""
+}
+
+
